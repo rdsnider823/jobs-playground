397941ddb41f37e32629ca9663fb9a2559c2263a
Luke Roling <luke@earnbetter.com>
2024-01-30 22:19:01 +0000

    tweaks

--- jobs_playground/jobs/api/views.py
+++ jobs_playground/jobs/api/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -22,7 +23,7 @@ class JobViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericVi
 
         params = self.request.query_params
 
-        job_types = [type for type in params.get("job_types", "").split(",")]
+        job_types = [type for type in params.get("job_types", "").split(",") if type]
         if len(job_types) > 0:
             queryset = queryset.filter(job_type__in=job_types)
 
